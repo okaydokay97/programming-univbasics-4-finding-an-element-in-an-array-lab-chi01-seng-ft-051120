49762e9882e73ed8d601c083f9436126719fcba7
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-21 14:34:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   
   while i < array.length do 
     if array[i] == value_to_find
-      found_value_index = count
+      found_value_index = i
     end
     i += 1 
   end
